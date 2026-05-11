bdcebcaece5982b6f0f20f6432cd7661d774b1b9
unknown <jigyasu2019india@gmail.com>
2026-05-11 10:03:18 +0400

    initial project setup is done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Jigyasu",
+    author_email="jigyasu2019india@gmail.com",
+    packages=find_packages()
+)
